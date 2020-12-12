0ab1a1bc920ca43962aeeda52bbc41274aca4547
angela2333 <angelawang2333@gmail.com>
2020-12-12 17:31:16 +0800

    add sql file

---
+++ test.sql
@@ -0,0 +1,6 @@
+select partner_id, file_name_matcher, map 
+from 
+    (select *, row_number()over(partition by partner_id, file_name_matcher order by created_at desc) as row_number
+     from column_maps 
+     where partner_id = 343) x 
+where tombstone <> true and row_number = 1
